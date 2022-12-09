8f149a733ba4a89c70d06edb8d59be4380672b0b
luis <lferreira646@gmail.com>
2022-12-09 19:51:44 -0300

    login form, login request

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -1,27 +1,58 @@
 import './styles.css';
+import { useState } from 'react';
+import { CredentialsDTO } from '../../../models/auth';
+import * as authService from '../../../services/auth-service';
 
 export default function Login() {
+
+  const [formData, setFormData] = useState<CredentialsDTO>({
+    username: '',
+    password: ''
+  });
+
+  function handleSubmit(event: any) {
+      event.preventDefault();
+      authService.loginRequest(formData)
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.log("Error no login", error);
+          })
+  }
+
+  function handleInputChange(event : any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({...formData, [name]: value});
+  }
 
     return(
         <main>
       <section id="login-section" className="dsc-container">
         <div className="dsc-login-form-container">
-          <form className="dsc-card dsc-form">
+          <form className="dsc-card dsc-form" onSubmit={handleSubmit}>
             <h2>Login</h2>
             <div className="dsc-form-controls-container">
               <div>
                 <input 
+                    name="username"
+                    value={formData.username}
                     className="dsc-form-control" 
                     type="email" 
                     placeholder="Email"
+                    onChange={handleInputChange}
                 />
                 <div className="dsc-form-error"></div>
               </div>
               <div>
                 <input 
+                    name="password"
+                    value={formData.password}
                     className="dsc-form-control" 
                     type="password" 
                     placeholder="Senha"
+                    onChange={handleInputChange}
                 />
               </div>
  
---
+++ src/services/auth-service.ts
@@ -0,0 +1,26 @@
+import { AxiosRequestConfig } from "axios";
+import QueryString from "qs";
+import { CredentialsDTO } from "../models/auth";
+import { requestBackend } from "../utils/requests";
+import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
+
+export function loginRequest(loginData: CredentialsDTO) {
+
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
+
+    }
+
+    const requestBody = QueryString.stringify({...loginData, grant_type: "password" });
+
+    const config: AxiosRequestConfig = {
+        method: "POST",
+        url: "/oauth/token",
+        data: requestBody,
+        headers
+    }
+
+    return requestBackend(config);
+
+}
