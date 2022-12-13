532c20031521c10974a432a7357dcd3f8bdc647b
luis <lferreira646@gmail.com>
2022-12-13 17:30:48 -0300

    Error  CSS

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -21,7 +21,7 @@ export default function ProductForm() {
       placeholder: "Nome",
     },
     price: {
-      value: 200,
+      value: "",
       id: "price",
       name: "price",
       type: "number",
@@ -43,9 +43,6 @@ export default function ProductForm() {
   useEffect(() => {
     if(isEditing) {
 
-      const obj = forms.validate(formData, "price");
-      console.log(obj)
-
       productService.findById(Number(params.productId))
         .then(response => {
           setFormData(forms.updateAll(formData, response.data));
@@ -54,7 +51,9 @@ export default function ProductForm() {
   }, []);
 
   function handleInputChange(event: any) {
-    setFormData(forms.update(formData,  event.target.name, event.target.value));
+    const dataUpdated = forms.update(formData,  event.target.name, event.target.value);
+    const dataValidated = forms.validate(dataUpdated, event.target.name);
+    setFormData(dataValidated);
   }
 
     return(
@@ -70,6 +69,7 @@ export default function ProductForm() {
                       className="dsc-form-control"
                       onChange={handleInputChange}
                     />
+                    <div className="dsc-form-error">{formData.name.message}</div>
                 </div>
                 <div>
                     <FormInput
@@ -77,6 +77,7 @@ export default function ProductForm() {
                       className="dsc-form-control"
                       onChange={handleInputChange}
                     />
+                    <div className="dsc-form-error">{formData.price.message}</div>
                 </div>
                 <div>
                     <FormInput
