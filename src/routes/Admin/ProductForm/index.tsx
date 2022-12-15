5800fd97b03be21cdc4288bdba9a742ef389005c
luis <lferreira646@gmail.com>
2022-12-15 11:46:16 -0300

    Backend errors

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -116,6 +116,10 @@ export default function ProductForm() {
 
     request.then(() => {
       navigate("/admin/products");
+    })
+    .catch(error => {
+      const newinputs = forms.setBackendErrors(formData, error.response.data.errors);
+      setFormData(newinputs);
     });
   }
 
